997a4d7091aad98f259b59eeb2c2e8adc848a755
MarkEEaton <MarkEEaton@users.noreply.github.com>
2016-07-29 00:05:32 -0400

    code from 2nd meeting
    very rough; not exactly working

---
+++ datafetch.py
@@ -0,0 +1,35 @@
+import json
+import requests
+import pickle
+
+users = ['users go here']
+
+
+def extractuserdata():
+    url = "https://api.github.com/users/{}"
+    return [json.loads(requests.get(url.format(user)).text) for user in users]
+
+
+def extractrepodata():
+    repodata = []
+    repojson = []
+
+    for user in users:
+        repodata.append(requests.get("https://api.github.com/users/{}/repos".format(user)))
+
+    for user in repodata:
+        repojson.append(json.loads(user.text))
+
+    return repojson
+
+
+def pickleit():
+    f1 = open('user.json', 'wb')
+    f2 = open('repo.json', 'wb')
+    pickle.dump(extractuserdata(), f1)
+    pickle.dump(extractrepodata(), f2)
+    f1.close()
+    f2.close()
+
+#pickleit()
+print(extractuserdata())
